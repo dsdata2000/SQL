835166bd4c6290ec64a220b50d00e2a7dd9c63a9
dsdata2000 <dsdata2000@gmail.com>
2022-07-11 11:30:58 -0400

    query-age-e2

--- creditcard_pay_trend_age_e2.sql
+++ creditcard_pay_trend_age_e2.sql
@@ -13,7 +13,7 @@ use test;
 describe Creditcard_feature_ext; 
 
 set @a1 = (select count(*) from Creditcard_feature_ext where (AGE<=35 and EDUCATION=2));
-set @a2 = (select count(*) from Creditcard_feature_ext where (AGE>35 and EDUCATION<=55 and EDUCATION=2));
+set @a2 = (select count(*) from Creditcard_feature_ext where (AGE>35 and AGE<=55 and EDUCATION=2));
 set @a3 = (select count(*) from Creditcard_feature_ext where (AGE>55 and EDUCATION=2));
 
 -- AGE-GROUP-I ---------------------------------------
@@ -62,11 +62,29 @@ group by PAYB_5 having PAYB_5=1;
 
 -- AGE-GROUP-II --------------------------------------------------
 
+alter table trend_age_edu add column q float not null after m;
+insert into trend_age_edu(month_, n, m, q)
+select "APR", 0, 0, count(PAYB_0)/@a3 from (select * from Creditcard_feature_ext where (AGE>55 and EDUCATION=2)) sub
+group by PAYB_0 having PAYB_0=1;
+insert into trend_age_edu(month_, n, m, q)
+select "MAY", 0, 0, count(PAYB_1)/@a3 from (select * from Creditcard_feature_ext where (AGE>55 and EDUCATION=2)) sub
+group by PAYB_1 having PAYB_1=1;
+insert into trend_age_edu(month_, n, m, q)
+select "JUNE", 0, 0, count(PAYB_2)/@a3 from (select * from Creditcard_feature_ext where (AGE>55 and EDUCATION=2)) sub
+group by PAYB_2 having PAYB_2=1;
+insert into trend_age_edu(month_, n, m, q)
+select "JULY", 0, 0, count(PAYB_3)/@a3 from (select * from Creditcard_feature_ext where (AGE>55 and EDUCATION=2)) sub
+group by PAYB_3 having PAYB_3=1;
+insert into trend_age_edu(month_, n, m, q)
+select "AUG", 0, 0, count(PAYB_4)/@a3 from (select * from Creditcard_feature_ext where (AGE>55 and EDUCATION=2)) sub
+group by PAYB_4 having PAYB_4=1;
+insert into trend_age_edu(month_, n, m, q)
+select "SEPT", 0, 0, count(PAYB_5)/@a3 from (select * from Creditcard_feature_ext where (AGE>55 and EDUCATION=2)) sub
+group by PAYB_5 having PAYB_5=1;
+
+-- result ----------
+
 select * from trend_age_edu;
-
-
--- select @a1 from Creditcard_feature_ext limit 1; 
-
 
 
 
